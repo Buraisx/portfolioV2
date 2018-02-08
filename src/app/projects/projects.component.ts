c21b22c3a827ab7f4153e7813a5ab6ec61e3a750
Situ William <situ_william@yahoo.com>
2018-02-08 13:38:25 -0500

    fixed most bugs, except gridlist responsiveness

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -9,12 +9,14 @@ import { Observable } from 'rxjs/Observable';
 })
 export class ProjectsComponent implements OnInit {
 	projectsObservable: Observable<any[]>;
+	columns = 2;
 	stack = ['rails','react','angular','node','bootstrap','postgres'];
   	constructor(private db: AngularFireDatabase) { }
 
 	ngOnInit() {
 		this.projectsObservable = this.getProjects('/projects');
 	}
+
 
 	getProjects(listPath): Observable<any[]>{
 		return this.db.list(listPath).valueChanges();
